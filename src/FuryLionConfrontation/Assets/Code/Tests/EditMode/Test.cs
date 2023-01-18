544dedb0e9bdfd395f6a0a88001c384751ace143
rapushka <mikalajsiatkouski@gmail.com>
2023-01-18 18:50:19 +0300

    working test!

--- src/FuryLionConfrontation/Assets/Code/Tests/EditMode/Test.cs
+++ src/FuryLionConfrontation/Assets/Code/Tests/EditMode/Test.cs
@@ -1,8 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Confrontation;
+using FluentAssertions;
 using NUnit.Framework;
 using UnityEngine;
-using UnityEngine.TestTools;
 
 public class Test
 {
@@ -10,11 +11,17 @@ public class Test
 	public void WhenGeneratedField_AndIsWasEmpty_ThenShouldNotContainNullCells()
 	{
 		// Arrange.
+		var prefab = new GameObject().AddComponent<Cell>();
+		var field = new Field(prefab);
+		var cells = (Cell[,])typeof(Field)
+		                     .GetField("_cells", BindingFlags.Instance | BindingFlags.NonPublic)
+		                     !.GetValue(field);
 
 		// Act.
+		field.Initialize();
 
 		// Assert.
-		
+		var containNullElements = cells.Cast<Cell>().Any((c) => c is null);
+		containNullElements.Should().BeFalse();
 	}
-
 }
