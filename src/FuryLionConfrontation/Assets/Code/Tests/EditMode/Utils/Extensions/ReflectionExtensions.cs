4d13167c21de45fb37efb45d4ebe71f9f5496b70
rapushka <mikalajsiatkouski@gmail.com>
2023-01-18 19:04:35 +0300

    FieldExtensions remaked to ReflectionExtensions

---
+++ src/FuryLionConfrontation/Assets/Code/Tests/EditMode/Utils/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using Confrontation;
+
+public static class ReflectionExtensions
+{
+	private const string Cells = "_cells";
+
+	public static Cell[,] GetCells(this Field @this) => @this.GetPrivate<Cell[,]>(Cells);
+	
+	public static T GetPrivate<T>(this object @this, string fieldName)
+		=> @this.GetFieldValue<T>(fieldName, BindingFlags.Instance | BindingFlags.NonPublic);
+	
+	public static T GetFieldValue<T>(this object @this, string fieldName, BindingFlags flags)
+		=> (T)@this.GetType().GetField(fieldName, flags)!.GetValue(@this);
+}
