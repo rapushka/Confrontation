cf1d72b6adf0ecf2574dfbff63883355c38385bc
rapushka <mikalajsiatkouski@gmail.com>
2023-01-24 11:00:53 +0300

    +Building info window

--- src/FuryLionConfrontation/Assets/Code/View/UI/Windows/Common/WindowBackground.cs
+++ src/FuryLionConfrontation/Assets/Code/View/UI/Windows/Common/WindowBackground.cs
@@ -3,10 +3,18 @@ using UnityEngine.EventSystems;
 
 namespace Confrontation
 {
-	public class WindowBackground : MonoBehaviour, IPointerClickHandler
+	public class WindowBackground : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 	{
 		[SerializeField] private WindowBase _window;
 
-		public void OnPointerClick(PointerEventData eventData) => _window.Hide();
+		public void OnPointerClick(PointerEventData eventData)
+		{
+			Debug.Log("Click");
+			_window.Hide();
+		}
+
+		public void OnPointerDown(PointerEventData eventData) => Debug.Log("Down");
+
+		public void OnPointerUp(PointerEventData eventData) => Debug.Log("Up");
 	}
 }
