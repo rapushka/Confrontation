ae45adc73f29aca74092ef5e36c9316544b4975d
rapushka <mikalajsiatkouski@gmail.com>
2023-02-05 00:43:38 +0300

    colorize regions

--- src/FuryLionConfrontation/Assets/Code/Editor/LevelEditor/CellsRegionGizmoDrawer.cs
+++ src/FuryLionConfrontation/Assets/Code/Editor/LevelEditor/CellsRegionGizmoDrawer.cs
@@ -17,10 +17,24 @@ namespace Confrontation.Editor
 		[DrawGizmo(GizmoType.Selected | GizmoType.NonSelected)]
 		private static void DrawGizmoForMyScript(Cell cell, GizmoType gizmoType)
 		{
-			if (_drawGizmosForCellsRegions)
+			if (_drawGizmosForCellsRegions == false)
 			{
-				Gizmos.DrawSphere(cell.transform.position, 0.25f);
+				return;
 			}
+
+			if (cell.RelatedRegion == true)
+			{
+				Gizmos.color = cell.RelatedRegion.OwnerPlayerId switch
+				{
+					0     => Color.white,
+					1     => Color.cyan,
+					2     => Color.red,
+					3     => Color.yellow,
+					var _ => Random.ColorHSV(),
+				};
+			}
+
+			Gizmos.DrawSphere(cell.transform.position, 0.25f);
 		}
 	}
 }
