0c4f026e314dc0bcd398409ef0cf32e5d3709ef2
rapushka <mikalajsiatkouski@gmail.com>
2023-02-05 19:58:12 +0300

    Building of Barracks and GoldenMines

--- src/FuryLionConfrontation/Assets/Code/Editor/LevelEditor/BuildingsCreator.cs
+++ src/FuryLionConfrontation/Assets/Code/Editor/LevelEditor/BuildingsCreator.cs
@@ -17,11 +17,17 @@ namespace Confrontation.Editor
 
 		private static IEnumerable<Cell> SelectedCells => Selection.gameObjects.WithComponent<Cell>();
 
+		private Barracks BarrackPrefab => _resources.Buildings.OfType<Barracks>().Single();
+
+		private GoldenMine GoldenMinePrefab => _resources.Buildings.OfType<GoldenMine>().Single();
+
 		public void GuiRender()
 		{
 			GUILayout.Label("Actions Perform to Selected Cells");
 			GUILayout.Button(nameof(BuildCapital).Pretty()).OnClick(BuildCapital);
 			GUILayout.Button(nameof(BuildVillage).Pretty()).OnClick(BuildVillage);
+			GUILayout.Button(nameof(BuildBarrack).Pretty()).OnClick(BuildBarrack);
+			GUILayout.Button(nameof(BuildGoldenMine).Pretty()).OnClick(BuildGoldenMine);
 			GUILayout.Button(nameof(DestroyBuilding).Pretty()).OnClick(DestroyBuilding);
 		}
 
@@ -29,11 +35,19 @@ namespace Confrontation.Editor
 
 		private void BuildVillage() => SelectedCells.Where((c) => c.IsEmpty).ForEach(BuildVillageOnCell);
 
+		private void BuildBarrack() => SelectedCells.Where((c) => c.IsEmpty).ForEach(BuildBarrackOnCell);
+
+		private void BuildGoldenMine() => SelectedCells.Where((c) => c.IsEmpty).ForEach(BuildGoldenMineOnCell);
+
 		private void DestroyBuilding() => SelectedCells.Where((c) => c.IsEmpty == false).ForEach(DestroyBuildingOnCell);
 
 		private void BuildCapitalOnCell(Cell cell) => BuildBuilding(cell, _resources.CapitalPrefab);
 
 		private void BuildVillageOnCell(Cell cell) => BuildBuilding(cell, _resources.VillagePrefab);
+
+		private void BuildBarrackOnCell(Cell cell) => BuildBuilding(cell, BarrackPrefab);
+
+		private void BuildGoldenMineOnCell(Cell cell) => BuildBuilding(cell, GoldenMinePrefab);
 
 		private void DestroyBuildingOnCell(Cell cell)
 		{
