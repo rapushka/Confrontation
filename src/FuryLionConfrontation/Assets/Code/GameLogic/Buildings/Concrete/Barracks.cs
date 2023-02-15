b2aa1c6ed97860d149128b9743a821644f58e610
rapushka <mikalajsiatkouski@gmail.com>
2023-02-16 02:19:06 +0300

    SpawnAmount usage

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Buildings/Concrete/Barracks.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Buildings/Concrete/Barracks.cs
@@ -10,7 +10,7 @@ namespace Confrontation
 
 		public float PassedDuration { get; set; }
 
-		public float CoolDownDuration => _balanceTable.Barracks[Level].CoolDownDuration;
+		public float CoolDownDuration => Balance.CoolDownDuration;
 
 		private bool HaveSquad => LocatedUnits is not null;
 
@@ -18,7 +18,15 @@ namespace Confrontation
 
 		private Vector3 InitialUnitPosition => transform.position + Constants.VerticalOffsetAboveCell;
 
-		public void Action() => CreateUnit();
+		private BarrackData Balance => _balanceTable.Barracks[Level];
+
+		public void Action()
+		{
+			for (var i = 0; i < Balance.SpawnAmount; i++)
+			{
+				CreateUnit();
+			}
+		}
 
 		private void CreateUnit()
 		{
