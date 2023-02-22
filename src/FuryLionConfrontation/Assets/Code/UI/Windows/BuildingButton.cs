7079252808fb5dfcc3757d9d864960b9680dcd73
rapushka <mikalajsiatkouski@gmail.com>
2023-02-23 01:43:00 +0300

    Show price of building in build menu

--- src/FuryLionConfrontation/Assets/Code/UI/Windows/BuildingButton.cs
+++ src/FuryLionConfrontation/Assets/Code/UI/Windows/BuildingButton.cs
@@ -8,10 +8,11 @@ namespace Confrontation
 	{
 		[Inject] private readonly Building _building;
 		[Inject] private readonly Purchase _purchase;
+		[Inject] private readonly IBalanceTable _balanceTable;
 
 		[SerializeField] private TextMeshProUGUI _textMesh;
 
-		private void Start() => _textMesh.text = _building.Name;
+		private void Start() => _textMesh.text = $"{_building.Name}\n{_balanceTable.PriceFor(_building)} G";
 
 		protected override void OnButtonClick() => _purchase.BuyBuilding(_building);
 
