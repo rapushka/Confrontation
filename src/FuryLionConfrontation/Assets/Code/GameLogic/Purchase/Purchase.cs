41405ca8d83340752ef4f1b370a3a9138c8e3cca
rapushka <mikalajsiatkouski@gmail.com>
2023-02-27 17:13:09 +0300

    upgrade as purchase

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Purchase/Purchase.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Purchase/Purchase.cs
@@ -7,14 +7,28 @@ namespace Confrontation
 		[Inject] private readonly BuildingSpawner _buildingSpawner;
 		[Inject] private readonly IBalanceTable _balanceTable;
 
-		public bool BuyBuilding(Player userPlayer, Building building, Cell locationCell)
+		public bool BuyBuilding(Player player, Building buildingPrefab, Cell locationCell)
 		{
-			var buildingPrice = _balanceTable.PriceFor(building);
+			var buildingPrice = _balanceTable.PriceFor(buildingPrefab);
 
-			if (userPlayer.Stats.IsEnoughGoldFor(buildingPrice))
+			if (player.Stats.IsEnoughGoldFor(buildingPrice))
 			{
-				_buildingSpawner.Build(building, locationCell);
-				userPlayer.Stats.Spend(buildingPrice);
+				_buildingSpawner.Build(buildingPrefab, locationCell);
+				player.Stats.Spend(buildingPrice);
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool UpgradeBuilding(Player player, Building building)
+		{
+			var upgradePrice = _balanceTable.PriceFor(building);
+
+			if (player.Stats.IsEnoughGoldFor(upgradePrice))
+			{
+				building.LevelUp();
+				player.Stats.Spend(upgradePrice);
 				return true;
 			}
 
--- src/FuryLionConfrontation/Assets/Code/UI/Windows/BuildingInfoWindow.cs
+++ src/FuryLionConfrontation/Assets/Code/UI/Windows/BuildingInfoWindow.cs
@@ -9,11 +9,18 @@ namespace Confrontation
 	public class BuildingInfoWindow : WindowBase
 	{
 		[Inject] private readonly IInputService _input;
+		[Inject] private readonly User _user;
+		[Inject] private readonly Purchase _purchase;
+		[Inject] private readonly BalanceTable _balanceTable;
+		[Inject] private readonly GameplayUiMediator _uiMediator;
 
 		[SerializeField] private TextMeshProUGUI _titleTextMesh;
 		[SerializeField] private Button _upgradeButton;
+		[SerializeField] private TextMeshProUGUI _upgradePriceTextMesh;
 
 		private Building _building;
+
+		private int UpgradePrice => _balanceTable.PriceFor(_building);
 
 		public override WindowBase Accept(IWindowVisitor windowVisitor) => windowVisitor.Visit(this);
 
@@ -36,13 +43,20 @@ namespace Confrontation
 
 		private void OnButtonClick()
 		{
-			_building.LevelUp();
-			UpdateView();
+			if (_purchase.UpgradeBuilding(_user.Player, _building))
+			{
+				UpdateView();
+			}
+			else
+			{
+				_uiMediator.OpenWindow<NotEnoughGoldWindow>();
+			}
 		}
 
 		private void UpdateView()
 		{
 			_titleTextMesh.text = _building.ToString();
+			_upgradePriceTextMesh.text = $"Upgrade ─ {UpgradePrice} G";
 			_upgradeButton.interactable = _building.IsOnMaxLevel == false;
 		}
 
