8da5e765033599f517b317a329e9e760d4a4529a
rapushka <mikalajsiatkouski@gmail.com>
2023-03-02 22:28:15 +0300

    remove coroutines usage

--- src/FuryLionConfrontation/Assets/Code/Infrastructure/Services/IRoutinesRunnerService.cs
+++ src/FuryLionConfrontation/Assets/Code/Infrastructure/Services/IRoutinesRunnerService.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Collections;
 using System.Threading;
 
 namespace Confrontation
 {
 	public interface IRoutinesRunnerService
 	{
-		void StartRoutine(IEnumerator routine);
-
 		void StopAllRoutines();
 
 		void StartRoutine(Action<CancellationTokenSource> cancelableTask);
