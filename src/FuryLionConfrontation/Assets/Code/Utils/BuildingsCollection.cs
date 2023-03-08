df9d56823f9282d2b0cf7566fc6842acabe5d8e8
rapushka <mikalajsiatkouski@gmail.com>
2023-03-08 15:29:36 +0300

    extracted BuildingsCollection

---
+++ src/FuryLionConfrontation/Assets/Code/Utils/BuildingsCollection.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Confrontation
+{
+	public static class BuildingsCollection
+	{
+		private static readonly List<(string Name, string Path)> _buildings
+			= new()
+			{
+				(nameof(Settlement), Constants.ResourcePath.Settlement),
+				(nameof(GoldenMine), Constants.ResourcePath.GoldenMine),
+				(nameof(Barrack), Constants.ResourcePath.Barrack),
+				(nameof(Capital), Constants.ResourcePath.Capital),
+				(nameof(Farm), Constants.ResourcePath.Farm),
+			};
+
+		public static string[] BuildingsNames => _buildings.Select((t) => t.Name).ToArray();
+
+		public static Building Load(int index) => Resources.Load<Building>(_buildings[index].Path);
+	}
+}
