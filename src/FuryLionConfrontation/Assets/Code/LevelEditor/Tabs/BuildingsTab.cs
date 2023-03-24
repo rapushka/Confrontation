b37cbf46116a3cb47e57ac3ff9ac626cf8f6db3d
rapushka <mikalajsiatkouski@gmail.com>
2023-03-24 04:15:33 +0300

    build page prefab

---
+++ src/FuryLionConfrontation/Assets/Code/LevelEditor/Tabs/BuildingsTab.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Confrontation
+{
+	public class BuildingsTab : LevelEditorPage
+	{
+		[SerializeField] private Transform _buildingButtonsRoot;
+
+		public override void Handle(Cell clickedCell) { }
+	}
+}
