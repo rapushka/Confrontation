de2b2e84d196ef0a50c2e15f3ae2fdb7842dfad4
rapushka <mikalajsiatkouski@gmail.com>
2023-03-28 20:16:03 +0300

    fix numbers issue

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Cells/BordersContainer.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Cells/BordersContainer.cs
@@ -25,8 +25,8 @@ namespace Confrontation
 				new BorderInfo(forEven: On(1, -1), forOdd: On(1, 0), border: _leftTop),
 				new BorderInfo(forEven: On(1, 0), forOdd: On(1, 1), border: _rightTop),
 				new BorderInfo(forEven: On(0, 1), forOdd: On(0, 1), border: _right),
-				new BorderInfo(forEven: On(1, 0), forOdd: On(-1, 1), border: _rightBottom),
-				new BorderInfo(forEven: On(-1, 1), forOdd: On(-1, 0), border: _leftBottom),
+				new BorderInfo(forEven: On(-1, 0), forOdd: On(-1, 1), border: _rightBottom),
+				new BorderInfo(forEven: On(-1, -1), forOdd: On(-1, 0), border: _leftBottom),
 			};
 
 			_borderInfos.Select((bi) => bi.Border).ForEach((b) => b.Hide());
