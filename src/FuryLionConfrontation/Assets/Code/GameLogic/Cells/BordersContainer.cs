d0cec7de11cc4834b27618e42074630a430cc504
rapushka <mikalajsiatkouski@gmail.com>
2023-03-28 20:55:55 +0300

    fixed

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Cells/BordersContainer.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Cells/BordersContainer.cs
@@ -17,36 +17,40 @@ namespace Confrontation
 
 		private BorderInfo[] _borderInfos;
 
+		private bool IsOnEvenRow => _cell.Coordinates.Row.IsEven();
+
 		private void OnEnable()
 		{
+			// https://www.redblobgames.com/grids/hexagons/#neighbors-offset — magic numbers explanation (greed type: odd-r)
 			_borderInfos = new[]
 			{
-				new BorderInfo(forEven: On(0, -1), forOdd: On(0, -1), border: _left),
-				new BorderInfo(forEven: On(1, -1), forOdd: On(1, 0), border: _leftTop),
-				new BorderInfo(forEven: On(1, 0), forOdd: On(1, 1), border: _rightTop),
-				new BorderInfo(forEven: On(0, 1), forOdd: On(0, 1), border: _right),
-				new BorderInfo(forEven: On(-1, 0), forOdd: On(-1, 1), border: _rightBottom),
-				new BorderInfo(forEven: On(-1, -1), forOdd: On(-1, 0), border: _leftBottom),
+				new BorderInfo(forEven: On(+0, -1), forOdd: On(+0, -1), border: _left),
+				new BorderInfo(forEven: On(+1, -1), forOdd: On(+1, +0), border: _leftTop),
+				new BorderInfo(forEven: On(+1, +0), forOdd: On(+1, +1), border: _rightTop),
+				new BorderInfo(forEven: On(+0, +1), forOdd: On(+0, +1), border: _right),
+				new BorderInfo(forEven: On(-1, +0), forOdd: On(-1, +1), border: _rightBottom),
+				new BorderInfo(forEven: On(-1, -1), forOdd: On(-1, +0), border: _leftBottom),
 			};
 
 			_borderInfos.Select((bi) => bi.Border).ForEach((b) => b.Hide());
 		}
 
-		private static Coordinates On(int row, int column) => new(row, column);
-
-		private bool IsOnEvenRow => _cell.Coordinates.Row.IsEven();
+		private static Coordinates On(int row, int column) => new(row: row, column: column);
 
 		public void SetBorderFor(Cell otherCell)
 		{
-			if (_cell.RelatedRegion == otherCell.RelatedRegion)
+			if (_cell.RelatedRegion != otherCell.RelatedRegion)
 			{
-				return;
+				_borderInfos.ForEach(ShowBorder, @if: (bi) => IsNeighbour(otherCell, bi));
 			}
-
-			var delta = _cell.Coordinates - otherCell.Coordinates;
-
-			_borderInfos.ForEach((bi) => bi.Border.Show(), @if: (bi) => delta == GetActualDelta(bi));
 		}
+
+		private static void ShowBorder(BorderInfo borderInfo) => borderInfo.Border.Show();
+
+		private bool IsNeighbour(Cell otherCell, BorderInfo borderInfo)
+			=> CurrentDelta(otherCell) == GetActualDelta(borderInfo);
+
+		private Coordinates CurrentDelta(Cell otherCell) => otherCell.Coordinates - _cell.Coordinates;
 
 		private Coordinates GetActualDelta(BorderInfo borderInfo)
 			=> IsOnEvenRow ? borderInfo.ForEven : borderInfo.ForOdd;
