5a36a4484c18bb30840b888c80f131f3a059f8fe
rapushka <mikalajsiatkouski@gmail.com>
2023-03-30 15:12:01 +0300

    +parameters in balance

--- src/FuryLionConfrontation/Assets/Code/StaticData/Balance/Stats/Specific/UnitStats.cs
+++ src/FuryLionConfrontation/Assets/Code/StaticData/Balance/Stats/Specific/UnitStats.cs
@@ -7,5 +7,11 @@ namespace Confrontation
 	public class UnitStats : IStats
 	{
 		[field: SerializeField] public float BaseSpeed { get; private set; }
+
+		[field: SerializeField] public float BaseStrength { get; private set; }
+
+		[field: Range(0f, 1f)] [field: SerializeField] public float DefenseModifier { get; private set; }
+
+		[field: Range(0f, 1f)] [field: SerializeField] public float AttackModifier { get; private set; }
 	}
 }
