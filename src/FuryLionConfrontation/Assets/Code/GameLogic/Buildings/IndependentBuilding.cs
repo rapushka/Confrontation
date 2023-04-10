cd11b74ff3448913e7430aad00077573f523301e
rapushka <mikalajsiatkouski@gmail.com>
2023-04-10 14:36:34 +0300

    semi works

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Buildings/IndependentBuilding.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Buildings/IndependentBuilding.cs
@@ -4,19 +4,18 @@ namespace Confrontation
 {
 	public abstract class IndependentBuilding : Building
 	{
-		[Inject] private readonly IField _iField;
 		[Inject] private readonly Region.Factory _regionsFactory;
 
 		private Region _ownRegion;
 
-		public int PlayerOwnerId { get; set; }
+		public override int OwnerPlayerId { get; set; }
 
 		private void Start()
 		{
-			_ownRegion = _regionsFactory.Create(_iField.Regions[Coordinates].Id);
+			_ownRegion = _regionsFactory.Create(Field.Regions[Coordinates].Id);
 			_ownRegion.Coordinates = Coordinates;
-			_ownRegion.OwnerPlayerId = PlayerOwnerId;
-			_iField.Regions.Add(_ownRegion);
+			_ownRegion.OwnerPlayerId = OwnerPlayerId;
+			Field.Regions.Add(_ownRegion);
 		}
 	}
 }
