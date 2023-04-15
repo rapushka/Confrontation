65a8c8e11a9005760b95aa3c19209f03190cf283
rapushka <mikalajsiatkouski@gmail.com>
2023-04-15 21:55:35 +0300

    +class for only override needed behaviour

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluencedStatsTable.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluencedStatsTable.cs
@@ -1,37 +1,4 @@
-using Zenject;
-
 namespace Confrontation
 {
-	public class InfluencedStatsTable : IStatsTable
-	{
-		[Inject] private IStatsTable _decoratee;
-
-		public UnitStats UnitStats => _decoratee.UnitStats;
-
-		public GoldenMineStats GoldenMineStats => _decoratee.GoldenMineStats;
-
-		public TowerOfMagesStats TowerOfMagesStats => _decoratee.TowerOfMagesStats;
-
-		public BarrackStats BarrackStats => _decoratee.BarrackStats;
-
-		public SettlementStats SettlementStats => _decoratee.SettlementStats;
-
-		public ForgeStats ForgeStats => _decoratee.ForgeStats;
-
-		public FortStats FortStats => _decoratee.FortStats;
-
-		public QuarryStats QuarryStats => _decoratee.QuarryStats;
-
-		public WorkshopStats WorkshopStats => _decoratee.WorkshopStats;
-
-		public EnemiesStats EnemiesStats => _decoratee.EnemiesStats;
-
-		public TimeStats TimeStats => _decoratee.TimeStats;
-
-		public FarmStats FarmStats => _decoratee.FarmStats;
-
-		public StableStats StableStats => _decoratee.StableStats;
-
-		public int BuildPriceFor(Building building) => _decoratee.BuildPriceFor(building);
-	}
+	public class InfluencedStatsTable : StatsTableDecoratorBase { }
 }
