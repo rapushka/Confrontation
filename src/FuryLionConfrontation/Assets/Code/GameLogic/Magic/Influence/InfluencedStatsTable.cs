981a64efb5f4606009a058918223a62b9dffede7
rapushka <mikalajsiatkouski@gmail.com>
2023-04-15 22:32:20 +0300

    now unit's stats will be changed with influence

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluencedStatsTable.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluencedStatsTable.cs
@@ -1,16 +1,24 @@
+using System.Collections.Generic;
 using Zenject;
+using static Confrontation.InfluenceTarget;
 
 namespace Confrontation
 {
 	public class InfluencedStatsTable : StatsTableDecoratorBase, IInitializable
 	{
+		[Inject] private readonly IEnumerable<TargetedInfluence> _influences;
+
 		private UnitStats _unitStats;
 
 		public void Initialize()
 		{
-			_unitStats = base.UnitStats.Clone().With((us) => us.BaseSpeed *= 2f);
+			_unitStats = base.UnitStats.Clone();
 		}
 
-		public override UnitStats UnitStats => _unitStats;
+		public override UnitStats UnitStats => _unitStats
+			.With((us) => us.BaseSpeed = InfluenceUnitSpeed(us));
+
+		private float InfluenceUnitSpeed(IUnitStats stats) 
+			=> _influences.WithTarget(UnitsSpeed).Influence(stats.BaseSpeed);
 	}
 }
--- src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/TargetedInfluence.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/TargetedInfluence.cs
@@ -6,12 +6,7 @@ namespace Confrontation
 	[Serializable]
 	public class TargetedInfluence
 	{
-		[field: SerializeField] public Influence Influence          { get; private set; }
-		[field: SerializeField] public Target    TargetForInfluence { get; private set; }
-
-		public enum Target
-		{
-			UnitsSpeed,
-		}
+		[field: SerializeField] public Influence       Influence          { get; private set; }
+		[field: SerializeField] public InfluenceTarget TargetForInfluence { get; private set; }
 	}
 }
