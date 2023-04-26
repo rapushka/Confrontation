168bca2ebb9af0db8aac1effd860804c03c0eba0
rapushka <mikalajsiatkouski@gmail.com>
2023-04-26 06:08:08 +0300

    using HashSet instead of List

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluenceMediator.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluenceMediator.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 using Zenject;
 
 namespace Confrontation
@@ -9,30 +11,44 @@ namespace Confrontation
 		[Inject] private readonly InfluencesWithDuration _influencesWithDuration;
 		[Inject] private readonly OnMovingUnitsInfluencer.Factory _onMovingUnitsInfluencerFactory;
 
-		private readonly List<OnMovingUnitsInfluencer> _onMovingUnitsInfluencers = new();
+		private readonly HashSet<OnMovingUnitsInfluencer> _onMovingUnitsInfluencers = new();
 
 		public void CastSpell(ISpell spell)
 		{
-			if (spell.SpellType is SpellType.Temporary)
+			switch (spell.SpellType)
 			{
-				_influencesWithDuration.CastSpell(spell);
-			}
-			else if (spell.SpellType is SpellType.Active)
-			{
-				var newInfluencer = _onMovingUnitsInfluencerFactory.Create();
-				newInfluencer.CastSpell(spell);
-				_onMovingUnitsInfluencers.Add(newInfluencer);
+				case SpellType.Temporary:
+					_influencesWithDuration.CastSpell(spell);
+					break;
+				case SpellType.Active:
+				{
+					CastActiveSpell(spell);
+					break;
+				}
+				case SpellType.Permanent:
+					Debug.LogError("TODO: Permanent spells");
+					break;
+				default:
+					throw new ArgumentOutOfRangeException();
 			}
 		}
 
-		public float Influence(float on, InfluenceTarget withTarget) 
+		public float Influence(float on, InfluenceTarget withTarget)
 			=> _influencesWithDuration.Influence(on, withTarget);
 
 		public void LateTick() => ClearUnusedInfluencers();
 
-		public float Influence(float on, InfluenceTarget withTarget, UnitsSquad @for) 
+		public float Influence(float on, InfluenceTarget withTarget, UnitsSquad @for)
 			=> _onMovingUnitsInfluencers.Aggregate(on, (x, i) => i.Influence(on: x, withTarget, @for));
 
-		private void ClearUnusedInfluencers() => _onMovingUnitsInfluencers.RemoveIf((i) => i.HasInfluenced == false);
+		private void ClearUnusedInfluencers()
+			=> _onMovingUnitsInfluencers.RemoveWhere((i) => i.HasInfluenced == false);
+
+		private void CastActiveSpell(ISpell spell)
+		{
+			var newInfluencer = _onMovingUnitsInfluencerFactory.Create();
+			newInfluencer.CastSpell(spell);
+			_onMovingUnitsInfluencers.Add(newInfluencer);
+		}
 	}
 }
