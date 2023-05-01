68ca4d311a1c6b1a763e507f864a3b10566ce535
rapushka <mikalajsiatkouski@gmail.com>
2023-05-02 01:22:39 +0300

    decorators usage

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluenceMediator.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/Influence/InfluenceMediator.cs
@@ -11,37 +11,46 @@ namespace Confrontation
 		[Inject] private readonly OnAllUntilMovingUnitsInfluencer.Factory _onAllMovingUnitsInfluencerFactory;
 		[Inject] private readonly DuratedInfluencer.Factory _duratedInfluenceFactory;
 		[Inject] private readonly PermanentInfluencer.Factory _permanentInfluencerFactory;
+		[Inject] private readonly InfluencerBase.Factory _influencerBaseFactory;
 
 		private readonly HashSet<IInfluencer> _duratedInfluencers = new();
 		private readonly HashSet<IInfluencer> _conditionalInfluencers = new();
 		private readonly HashSet<IInfluencer> _permanentInfluencers = new();
 
-		public InfluenceStatus Status => InfluenceStatus.Neutral;
+		public InfluenceStatus Status => InfluenceStatus.ForceLive;
 
 		private IEnumerable<IInfluencer> CommonInfluencers => _duratedInfluencers.Concat(_permanentInfluencers);
 
 		public void CastSpell(ISpell spell)
 		{
-			switch (spell.SpellType)
+			foreach (var influence in spell.Influences)
 			{
-				case SpellType.Temporary:
-					_duratedInfluencers.AddRange(spell.Influences.Select((i) => AsDurated(i, spell.Duration)));
-					break;
-				case SpellType.Active:
-					_conditionalInfluencers.AddRange(spell.Influences.Select(AsInfluencerForTarget));
-					break;
-				case SpellType.Permanent:
-					_permanentInfluencers.AddRange(spell.Influences.Select(_permanentInfluencerFactory.Create));
-					break;
-				default:
-					throw new ArgumentOutOfRangeException();
+				IInfluencer influencerBase = _influencerBaseFactory.Create(influence);
+
+				if (spell.SpellType is SpellType.Temporary)
+				{
+					influencerBase = _duratedInfluenceFactory.Create(spell.Duration, influencerBase);
+					_duratedInfluencers.Add(influencerBase);
+				}
+
+				if (spell.SpellType is SpellType.Active)
+				{
+					influencerBase = AsInfluencerForTarget(influence, influencerBase);
+					_conditionalInfluencers.Add(influencerBase);
+				}
+
+				if (spell.SpellType is SpellType.Permanent)
+				{
+					influencerBase = _permanentInfluencerFactory.Create(influencerBase);
+					_permanentInfluencers.Add(influencerBase);
+				}
 			}
 		}
 
+		public void LateTick() => ClearUnusedInfluencers();
+
 		public float Influence(float on, InfluenceTarget withTarget)
 			=> CommonInfluencers.Aggregate(on, (current, i) => i.Influence(current, withTarget));
-
-		public void LateTick() => ClearUnusedInfluencers();
 
 		public float Influence<T>(float on, InfluenceTarget withTarget, T @for)
 		{
@@ -52,16 +61,10 @@ namespace Confrontation
 			return Influence(fromConditional, withTarget);
 		}
 
-		private void ClearUnusedInfluencers()
-		{
-			_conditionalInfluencers.RemoveWhere((i) => i.Status == InfluenceStatus.ForceDeath);
-			_duratedInfluencers.RemoveWhere((i) => i.Status == InfluenceStatus.ForceDeath);
-		}
-
-		private IInfluencer AsInfluencerForTarget(Influence influence)
+		private IInfluencer AsInfluencerForTarget(Influence influence, IInfluencer influencer)
 			=> influence.Target switch
 			{
-				AllUntillMovingUnitsSpeed => _onAllMovingUnitsInfluencerFactory.Create(influence),
+				AllUntillMovingUnitsSpeed => _onAllMovingUnitsInfluencerFactory.Create(influencer),
 				AllNowMovingUnitsStrength => throw new NotImplementedException(),
 				OurUnitsSpeed             => throw new NotImplementedException(),
 				AllFarmsBonus             => throw new NotImplementedException(),
@@ -72,7 +75,10 @@ namespace Confrontation
 				_                         => throw new ArgumentOutOfRangeException(),
 			};
 
-		private DuratedInfluencer AsDurated(Influence influence, float duration)
-			=> _duratedInfluenceFactory.Create(influence, duration);
+		private void ClearUnusedInfluencers()
+		{
+			_conditionalInfluencers.RemoveWhere((i) => i.Status == InfluenceStatus.ForceDeath);
+			_duratedInfluencers.RemoveWhere((i) => i.Status == InfluenceStatus.ForceDeath);
+		}
 	}
 }
