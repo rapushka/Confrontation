a17b8325fbe6e2a784e01235df4cc3b635b20ced
rapushka <mikalajsiatkouski@gmail.com>
2023-05-06 12:26:34 +0300

    game pause while there is tutorial window

--- src/FuryLionConfrontation/Assets/Code/Tutorial/Tutorial.cs
+++ src/FuryLionConfrontation/Assets/Code/Tutorial/Tutorial.cs
@@ -4,12 +4,10 @@ namespace Confrontation
 {
 	public class Tutorial : IInitializable
 	{
-		[Inject] private readonly TimeStopService _timeStopService;
 		[Inject] private readonly GameplayUiMediator _ui;
 
 		public void Initialize()
 		{
-			_timeStopService.Stop();
 			_ui.OpenWindow<TutorialWindow>();
 		}
 	}
--- src/FuryLionConfrontation/Assets/Code/Tutorial/TutorialWindow.cs
+++ src/FuryLionConfrontation/Assets/Code/Tutorial/TutorialWindow.cs
@@ -5,6 +5,20 @@ namespace Confrontation
 {
 	public class TutorialWindow : GameplayWindowBase
 	{
+		[Inject] private readonly TimeStopService _timeStopService;
+
+		public override void Open()
+		{
+			_timeStopService.Stop();
+			base.Open();
+		}
+
+		public override void Close()
+		{
+			base.Close();
+			_timeStopService.Resume();
+		}
+
 		public override GameplayWindowBase Accept(IGameplayWindowVisitor visitor) => visitor.Visit(this);
 
 		public new class Factory : PlaceholderFactory<Object, TutorialWindow> { }
