60f181b6618f043c12407a2101fc831ad0940846
rapushka <mikalajsiatkouski@gmail.com>
2023-05-06 14:15:47 +0300

    tabs system rework as dynamic

--- src/FuryLionConfrontation/Assets/Code/UI/TabsSystem/Tab.cs
+++ src/FuryLionConfrontation/Assets/Code/UI/TabsSystem/Tab.cs
@@ -7,6 +7,12 @@ namespace Confrontation
 	[Serializable]
 	public class Tab
 	{
+		public Tab(Button button, Page page)
+		{
+			Button = button;
+			Page = page;
+		}
+
 		[field: SerializeField] public Button Button { get; private set; }
 		[field: SerializeField] public Page   Page   { get; private set; }
 
