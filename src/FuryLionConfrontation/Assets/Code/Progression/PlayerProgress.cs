3602aac1ba6a1341f4effaa87f1e09a74a9e8f54
rapushka <mikalajsiatkouski@gmail.com>
2023-05-07 21:29:16 +0300

    +ProgressionService

---
+++ src/FuryLionConfrontation/Assets/Code/Progression/PlayerProgress.cs
@@ -0,0 +1,9 @@
+namespace Confrontation
+{
+	public struct PlayerProgress
+	{
+		public int CompletedLevelsCount { get; set; }
+		public int KalymCount           { get; set; }
+		public int LearnedSpellsCount   { get; set; }
+	}
+}
