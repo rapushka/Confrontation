6ea5ff71d23c0152626c6eea3c0bf8cb9d8c7652
rapushka <mikalajsiatkouski@gmail.com>
2023-05-07 21:48:14 +0300

    now available only completed levels +1

--- src/FuryLionConfrontation/Assets/Code/MetaGame/LevelButtonsSpawner.cs
+++ src/FuryLionConfrontation/Assets/Code/MetaGame/LevelButtonsSpawner.cs
@@ -7,14 +7,19 @@ namespace Confrontation
 	{
 		[Inject] private readonly List<LevelScriptableObject> _levels;
 		[Inject] private readonly LevelButtonBase.Factory _levelButtonsFactory;
+		[Inject] private readonly IProgressionService _progression;
 
 		private int _counter = 1;
+
+		private bool IsUnlocked => UnlockedLevelsCount >= _counter;
+
+		private int UnlockedLevelsCount => _progression.LoadProgress().CompletedLevelsCount + 1;
 
 		public void Initialize() => _levels.ForEach(level => Create(level));
 
 		protected virtual LevelButtonBase Create(ILevel level)
 		{
-			var levelButtonBase = _levelButtonsFactory.Create<LevelButtonBase>(_counter, level);
+			var levelButtonBase = _levelButtonsFactory.Create<LevelButtonBase>(_counter, level, IsUnlocked);
 			_counter++;
 			return levelButtonBase;
 		}
--- src/FuryLionConfrontation/Assets/Code/UI/Buttons/LevelButtonBase.cs
+++ src/FuryLionConfrontation/Assets/Code/UI/Buttons/LevelButtonBase.cs
@@ -21,11 +21,12 @@ namespace Confrontation
 
 		public class Factory : PlaceholderFactory<ILevel, LevelButtonBase>
 		{
-			public T Create<T>(int levelNumber, ILevel level)
+			public T Create<T>(int levelNumber, ILevel level, bool isActive = true)
 				where T : LevelButtonBase
 			{
 				var levelButton = base.Create(level);
 				levelButton._textMesh.text = levelNumber.ToString();
+				levelButton.Interactable = isActive;
 				return (T)levelButton;
 			}
 		}
