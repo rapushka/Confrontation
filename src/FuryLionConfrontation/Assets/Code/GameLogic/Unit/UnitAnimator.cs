2f2efc890ba07b88e9a71c983098c9ae85967dcc
rapushka <mikalajsiatkouski@gmail.com>
2023-05-07 13:02:03 +0300

    on stop time also stopping animations

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Unit/UnitAnimator.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Unit/UnitAnimator.cs
@@ -6,12 +6,18 @@ namespace Confrontation
 	public class UnitAnimator : MonoBehaviour
 	{
 		[Inject] private Animator _animator;
+		[Inject] private readonly ITimeService _time;
 
+		private const float AnimatorDefaultSpeed = 1.0f;
+		private const float StoppedAnimatorSpeed = 0.0f;
+		
 		public bool IsMoving
 		{
 			get => _animator.GetBool(Constants.AnimationHash.IsMoving);
 			private set => _animator.SetBool(Constants.AnimationHash.IsMoving, value);
 		}
+
+		private void Update() => _animator.speed = _time.DeltaTime > 0 ? AnimatorDefaultSpeed : StoppedAnimatorSpeed;
 
 		public void StartMoving() => IsMoving = true;
 
