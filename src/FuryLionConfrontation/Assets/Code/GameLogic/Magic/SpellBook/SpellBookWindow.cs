dca572fb9dbaa441961d7ef2ec431815ceebd54a
rapushka <mikalajsiatkouski@gmail.com>
2023-05-09 01:01:46 +0300

    now user can cast only unlocked spells

--- src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/SpellBook/SpellBookWindow.cs
+++ src/FuryLionConfrontation/Assets/Code/GameLogic/Magic/SpellBook/SpellBookWindow.cs
@@ -10,14 +10,19 @@ namespace Confrontation
 	{
 		[Inject] private readonly SpellButton.Factory _spellButtonFactory;
 		[Inject] private readonly TimeStopService _timeStopService;
+		[Inject] private readonly IProgressionStorageService _progression;
 
 		[SerializeField] private List<SpellScriptableObject> _spells;
 		[SerializeField] private Transform _buttonsRoot;
 		[SerializeField] private ToolTip _toolTip;
 
+		private float _spellsCounter;
+
+		private int LearnedSpellsCount => _progression.LoadProgress().LearnedSpellsCount;
+
 		private void Start()
 			=> _spells
-			   .Select((s) => _spellButtonFactory.Create(s, _toolTip))
+			   .Select((s) => _spellButtonFactory.Create(s, _toolTip, LearnedSpellsCount > _spellsCounter++))
 			   .ForEach((sb) => sb.transform.SetParent(_buttonsRoot));
 
 		public override void Open()
