d02a32f1ab3be7bc9750f960922d3e36e2c90319
rapushka <mikalajsiatkouski@gmail.com>
2023-05-09 12:51:59 +0300

    time stop on pause

--- src/FuryLionConfrontation/Assets/Code/UI/Windows/PauseWindow.cs
+++ src/FuryLionConfrontation/Assets/Code/UI/Windows/PauseWindow.cs
@@ -5,7 +5,21 @@ namespace Confrontation
 {
 	public class PauseWindow : GameplayWindowBase
 	{
+		[Inject] private readonly TimeStopService _timeStopService;
+
 		public override GameplayWindowBase Accept(IGameplayWindowVisitor visitor) => visitor.Visit(this);
+
+		public override void Open()
+		{
+			_timeStopService.Stop();
+			base.Open();
+		}
+
+		public override void Close()
+		{
+			base.Close();
+			_timeStopService.Resume();
+		}
 
 		public new class Factory : PlaceholderFactory<Object, PauseWindow> { }
 	}
